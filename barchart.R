327b8e19611f1b682e4ce2cad9a389ff31add368
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:45:06 -0400

    Saving barchart.R

--- barchart.R
+++ barchart.R
@@ -9,12 +9,12 @@ murders_per_state<-as.data.frame(cbind(state,murders_each_state))
 #View(murders_per_state)
 
 # Create a bar chart with the number of murders per state
-murder_bar_chart<-ggplot(murders_per_state,aes(x=state, y=murders)) +
+murder_bar_chart<-ggplot(murders_per_state,aes(x=state, y=murders_each_state)) +
   geom_col()
 murder_bar_chart
 
 # Create a bar chart with the number of murders per state on the adjusted axis
-murder_bar_chart2<-ggplot(murders_per_state,aes(x=state, y=murders)) +
+murder_bar_chart2<-ggplot(murders_per_state,aes(x=state, y=murders_each_state)) +
   geom_col() + 
   ggtitle("Total Murders") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
@@ -23,11 +23,11 @@ murder_bar_chart2
 
 # Create a bar chart with the number of murders per state on the x axis and sort the murder rate
 # Create a new dataframe with sorted murder data
-murder_data<-murders_per_state[order(murders_per_state$murders, decreasing= TRUE),]
+murder_data<-murders_per_state[order(murders_per_state$murders_each_state, decreasing= TRUE),]
 #create rows at 1
 rownames(murder_data)<-NULL
 #Create the bar graph
-murder_bar_chart3<-ggplot(murder_data,aes(x=reorder(state,-murders), y=murders)) +
+murder_bar_chart3<-ggplot(murder_data,aes(x=reorder(state,-murders_each_state), y=murders_each_state)) +
   geom_bar(stat = "identity") + 
   ggtitle("Total Murders") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
@@ -37,7 +37,7 @@ murder_bar_chart3
 #create dataframe to put popOver18 with murder_data
 murder_data$popOver18<-clean_data$popOver18
 #bar graph
-murder_bar_chart4<-ggplot(murder_data,aes(x=reorder(state,-murders), y=murders, group= 1, fill=popOver18)) +
+murder_bar_chart4<-ggplot(murder_data,aes(x=reorder(state,-murders_each_state), y=murders_each_state, group= 1, fill=popOver18)) +
   geom_bar(stat = "identity") + 
   ggtitle("Total Murders") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
