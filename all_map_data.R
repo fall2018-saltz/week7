de553b5e1279891ad4a06232aea17eb1c4d596c7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:19:29 -0400

    moving map_state_area.R to all_map_data.R

---
+++ all_map_data.R
@@ -0,0 +1,14 @@
+
+library("ggplot2")
+library("maps")
+library("ggmap")
+##Step B: Create a color coded map by state.area
+map.popColor <- ggplot(update_clean_data, aes(map_id = stateName))
+map.popColor <- map.popColor +
+  geom_map(map = us1, aes(fill=state.area))
+map.popColor <- map.popColor +
+  expand_limits(x = us1$long, y = us1$lat)
+map.popColor <- map.popColor +
+  scale_fill_gradient2(name=" Area of State", low = "blue", mid = "purple", high = "red")+
+  coord_map() + ggtitle("State Area")
+map.popColor
